7915d7f083a4da226f21195fa969249ad5d9e7c2
Apple <apple@Apples-MacBook-Pro-170.local>
2019-07-22 15:41:13 -0700

    All Day 1 Notes

--- KWK Day 1.playground/Contents.swift
+++ KWK Day 1.playground/Contents.swift
@@ -47,4 +47,70 @@ else {
     print ("try again")
 }
 
+//call you dog, get the collar, get the leash, put collar on your dog,attach leash to collar
+func walkDog () {
+    print("call your dog")
+    print("get leash")
+    print("attach leash")
+    print("open door")
+    print("walk out")
+    print("close door")
+    print("walk")
 }
+walkDog()
+
+func bowlCereal () {
+    print("get bowl")
+    print("get cereal")
+    print("get milk")
+    print("dump cereal into bowl")
+    print("pour milk")
+    print("grab spoon")
+}
+bowlCereal()
+
+func mornRou() {
+    print("wake up")
+    print("turn off alarm")
+    print("brush teeth")
+    print("get dressed")
+}
+func walkDog (numberOfDogs: Int) -> Int {
+    var lengthOfWalk = numberOfDogs * 15
+    return lengthOfWalk
+    print("There are \(numberOfDogs) dogs in the house")
+}
+
+let minutesToWalk = walkDog(numberOfDogs: 3)
+print(" Please walk the dog. I will expect to see you complete that task in \(minutesToWalk) minutes!")
+
+walkDog(numberOfDogs : 4)
+    
+func hello (whosName: String) {
+print(" hello \(whosName)")
+}
+
+hello(whosName : "Trinity")
+
+func myAge() -> Int {
+    return 18
+}
+print(myAge() + 10)
+
+var arrayOfStrings = [String] ()
+var friendsOfKarlie = ["Michelle Obama", " Serena Williams", "T Swift", "Jimmy Fallon"]
+friendsOfKarlie
+friendsOfKarlie[2]
+friendsOfKarlie[0]
+print(friendsOfKarlie[0])
+friendsOfKarlie[2] = "Josh Kushner"
+friendsOfKarlie
+print(friendsOfKarlie[2])
+
+var arraOfStrings = [String] ()
+var roleModel = ["RBG", "Lana Condor", "Jenny Han"]
+roleModel
+roleModel.append("Yara Shadidi")
+roleModel[1] = " Michele Obama"
+
+
