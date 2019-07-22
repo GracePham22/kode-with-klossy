7ee0918b9443475eb7052cca0416cda72efd1aa4
Apple <apple@Apples-MacBook-Pro-170.local>
2019-07-22 12:57:53 -0700

    my first playground

---
+++ KWK Day 1.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+str
+var name = "Grace"
+name
+print("Hello World!")
+var age = "15"
+print(age)
+print("I am \(age)")
+print("I am happy")
+var first = "Karlie"
+var last = "Klossy"
+print("\(first)\(last)")
+print("\(last)\(first)")
+print("\(first) \(last)")
+var a = 12.0
+var b = 65.0
+var c = 31.0
+var d = 98.0
+(a + b + c + d) / 4
+5 < 3
+12 > 7
+6 != 8
+//7 == "7"
+"karlie" == "karlie"
+var dogAge = 1
+if dogAge < 2 {
+    print( "You are a puppy")
+}
+//else if dogAge > 12 {
+    //print( "You are an ederly")
+//}
+var favFood = "ice cream"
+if favFood == "ice cream" {
+    print( " Yummy")
+}
+else if favFood == "ice cream" {
+    print( " vanilla")
+}
