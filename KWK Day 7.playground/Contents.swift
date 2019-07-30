c6ebb66d624a97ed3015a08872d2d964a060d2ad
Apple <apple@Apples-MacBook-Pro-170.local>
2019-07-30 16:40:21 -0700

    Day7

---
+++ KWK Day 7.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+var number = Array (1...100)
+print(number)
+
+for number in 1...100 {
+    
+    if number%1 == 0 && number <= 100 {
+        print(number)
+    }
+        if number isMultiple(of: 3) {
+            print("Fizz")
+        }
+    }
+
+
